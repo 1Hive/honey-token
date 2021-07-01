a97a1ebba6667b44df148bf910f3ecc9024c59cb
William Griffiths <willjgriff@gmail.com>
2021-07-01 12:46:16 +0100

    Continue working on L1 issuance.

--- contracts/l1Issuance/L1Issuance.sol
+++ contracts/l1Issuance/L1Issuance.sol
@@ -5,34 +5,50 @@ import '../Honey.sol';
 contract L1Issuance {
 
     Honey public honey;
-    address public issuanceAddress;
+    address public l2IssuanceAddress;
+    address public l2GovernanceAddress;
+    mapping(uint256 => bool) public issuanceRequestIds;
 
     modifier onlyIssuanceFromL2 {
         // verify caller
         _;
     }
 
-    constructor(Honey _honey, address _issuanceAddress) public {
+    modifier onlyGovernanceFromL2 {
+        // verify caller
+        _;
+    }
+
+    constructor(Honey _honey, address _l2IssuanceAddress, address _l2GovernanceAddress) public {
         honey = _honey;
-        issuanceAddress = _issuanceAddress;
+        l2IssuanceAddress = _l2IssuanceAddress;
+        l2GovernanceAddress = _l2GovernanceAddress;
     }
 
-    function updateIssuanceAddress(address _issuanceAddress) public {
-        issuanceAddress = _issuanceAddress;
+    function updateIssuanceAddress(address _l2IssuanceAddress) external onlyGovernanceFromL2 {
+        l2IssuanceAddress = _l2IssuanceAddress;
     }
 
-    function mintHoney(uint256 _issuanceId, uint256 _amount) external onlyIssuanceFromL2 {
+    function updateGovernanceAddress(address _l2GovernanceAddress) external onlyGovernanceFromL2 {
+        l2GovernanceAddress = _l2GovernanceAddress;
+    }
+
+    function mintHoney(uint256 _issuanceRequestId, uint256 _amount) external onlyIssuanceFromL2 {
         honey.mint(address(this), _amount);
         // send minted honey to L2Issuance
         // call finalize on L2Issuance
     }
 
     // Expects to have received _amount before executing
-    function burnHoney(uint256 _issuanceId, uint256 _amount) external onlyIssuanceFromL2 {
+    function burnHoney(uint256 _issuanceRequestId, uint256 _amount) external onlyIssuanceFromL2 {
         require(honey.balanceOf(address(this)) >= _amount, "ERROR: No burn balance");
         honey.burn(_amount);
         // call finalize on L2Issuance
     }
 
+    function finalizeL2Issuance(uint256 _issuanceRequestId) external {
+        // call finalise on L2Issuance
+    }
 
+    function _finalizeL2Issuance()
 }
