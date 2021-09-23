239b78ad2b24b30532dc90ffcc91c7dd871eeb65
William Griffiths <willjgriff@gmail.com>
2021-09-23 14:50:41 +0100

    Complete initial tests for ArbitrumBridgeReceiver contract

--- test/arbitrum/ArbitrumBridgeReceiver.js
+++ test/arbitrum/ArbitrumBridgeReceiver.js
@@ -10,76 +10,175 @@ const ArbitrumInbox = artifacts.require("ArbitrumInboxMock")
 const ArbitrumGatewayRouter = artifacts.require("ArbitrumGatewayRouterMock")
 
 contract('ArbitrumBridgeReceiver', ([deployer, l2Issuance, l2Governance, newAddress, gatewayRouter, gateway]) => {
+  context.only('Contract', () => {
 
-  let honey, arbitrumOutbox, arbitrumBridge, arbitrumInbox, arbitrumGatewayRouter, arbitrumBridgeReceiver
+    let honey, arbitrumOutbox, arbitrumBridge, arbitrumInbox, arbitrumGatewayRouter, arbitrumBridgeReceiver
 
-  beforeEach(async () => {
-    honey = await Honey.new(deployer, deployer, gatewayRouter, gateway)
-    arbitrumOutbox = await ArbitrumOutbox.new(l2Issuance)
-    arbitrumBridge = await ArbitrumBridge.new(arbitrumOutbox.address)
-    arbitrumInbox = await ArbitrumInbox.new(arbitrumBridge.address)
-    arbitrumGatewayRouter = await ArbitrumGatewayRouter.new(honey.address, gateway)
+    const honeySupply = bigExp(1000000, 18)
 
-    arbitrumBridgeReceiver = await ArbitrumBridgeReceiver.new(honey.address, l2Issuance, l2Governance,
-      arbitrumInbox.address, arbitrumGatewayRouter.address)
-    await honey.changeIssuer(arbitrumBridgeReceiver.address)
-    await honey.changeGatewaySetter(arbitrumBridgeReceiver.address)
-  })
+    beforeEach(async () => {
+      honey = await Honey.new(deployer, deployer, gatewayRouter, gateway)
+      arbitrumOutbox = await ArbitrumOutbox.new(deployer)
+      arbitrumBridge = await ArbitrumBridge.new(arbitrumOutbox.address)
+      arbitrumInbox = await ArbitrumInbox.new(arbitrumBridge.address)
+      arbitrumGatewayRouter = await ArbitrumGatewayRouter.new(honey.address, gateway)
 
-  it('sets correct constructor params', async () => {
-    assert.equal(await arbitrumBridgeReceiver.honey(), honey.address, "Incorrect honey address")
-    assert.equal(await arbitrumBridgeReceiver.l2IssuanceAddress(), l2Issuance, "Incorrect l2IssuanceAddress address")
-    assert.equal(await arbitrumBridgeReceiver.l2GovernanceAddress(), l2Governance, "Incorrect l2GovernanceAddress address")
-    assert.equal(await arbitrumBridgeReceiver.arbitrumInbox(), arbitrumInbox.address, "Incorrect arbitrumInbox address")
-    assert.equal(await arbitrumBridgeReceiver.arbitrumGatewayRouter(), arbitrumGatewayRouter.address, "Incorrect arbitrumGatewayRouter address")
-  })
-
-  const itUpdatesAndRevertsAsExpected = (updateAddressFunction, fieldToCheck) => {
-    it('updates address as expected', async () => {
-      await arbitrumOutbox.setL2ToL1Sender(l2Governance)
-      const callDataNewAddress = arbitrumBridgeReceiver.contract.methods[updateAddressFunction](newAddress).encodeABI()
-      await arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress)
-      assert.equal(await arbitrumBridgeReceiver[fieldToCheck](), newAddress, "Incorrect new address")
+      arbitrumBridgeReceiver = await ArbitrumBridgeReceiver.new(honey.address, l2Issuance, l2Governance,
+        arbitrumInbox.address, arbitrumGatewayRouter.address)
+      await honey.mint(gateway, honeySupply)
+      await honey.changeIssuer(arbitrumBridgeReceiver.address)
+      await honey.changeGatewaySetter(arbitrumBridgeReceiver.address)
     })
 
-    it('reverts when l2tol1sender is incorrect', async () => {
-      const callDataNewAddress = arbitrumBridgeReceiver.contract.methods[updateAddressFunction](newAddress).encodeABI()
-      await assertRevert(arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress), "ERR:NOT_GOVERNANCE")
+    it('sets correct constructor params', async () => {
+      assert.equal(await arbitrumBridgeReceiver.honey(), honey.address, "Incorrect honey address")
+      assert.equal(await arbitrumBridgeReceiver.l2IssuanceAddress(), l2Issuance, "Incorrect l2IssuanceAddress address")
+      assert.equal(await arbitrumBridgeReceiver.l2GovernanceAddress(), l2Governance, "Incorrect l2GovernanceAddress address")
+      assert.equal(await arbitrumBridgeReceiver.arbitrumInbox(), arbitrumInbox.address, "Incorrect arbitrumInbox address")
+      assert.equal(await arbitrumBridgeReceiver.arbitrumGatewayRouter(), arbitrumGatewayRouter.address, "Incorrect arbitrumGatewayRouter address")
     })
 
-    it('reverts when not called from bridge', async () => {
-      await assertRevert(arbitrumBridgeReceiver[updateAddressFunction](newAddress), "ERR:NOT_FROM_BRIDGE")
+    const itUpdatesBridgeReceiverAsExpected = (updateAddressFunction, fieldToCheck) => {
+      it('updates address as expected', async () => {
+        await arbitrumOutbox.setL2ToL1Sender(l2Governance)
+        const callDataNewAddress = arbitrumBridgeReceiver.contract.methods[updateAddressFunction](newAddress).encodeABI()
+        await arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress)
+        assert.equal(await arbitrumBridgeReceiver[fieldToCheck](), newAddress, "Incorrect new address")
+      })
+    }
+
+    const itUpdatesHoneyAsExpected = (updateAddressFunction, fieldToCheck) => {
+      it("updates issuance address on honey token", async () => {
+        await arbitrumOutbox.setL2ToL1Sender(l2Governance)
+        const callDataNewAddress = arbitrumBridgeReceiver.contract.methods[updateAddressFunction](newAddress).encodeABI()
+        await arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress)
+        assert.equal(await honey[fieldToCheck](), newAddress, 'Incorrect new address')
+      })
+    }
+
+    const itRevertsUpdateFunctionAsExpected = (updateAddressFunction) => {
+      it('reverts when l2tol1sender is incorrect', async () => {
+        const callDataNewAddress = arbitrumBridgeReceiver.contract.methods[updateAddressFunction](newAddress).encodeABI()
+        await assertRevert(arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress), "ERR:NOT_GOVERNANCE")
+      })
+
+      it('reverts when not called from bridge', async () => {
+        await assertRevert(arbitrumBridgeReceiver[updateAddressFunction](newAddress), "ERR:NOT_FROM_BRIDGE")
+      })
+
+      it('reverts when l2tol2sender is zero address', async () => {
+        await arbitrumOutbox.setL2ToL1Sender(ZERO_ADDRESS)
+        const callDataNewAddress = arbitrumBridgeReceiver.contract.methods[updateAddressFunction](newAddress).encodeABI()
+        await assertRevert(arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress), "ERR:NO_SENDER")
+      })
+    }
+
+    const itRevertsIssuanceFunctionAsExpected = (issuanceFunction) => {
+      const issueAmount = bigExp(123, 18)
+
+      it('reverts when l2tol1sender is incorrect', async () => {
+        const callDataNewAddress = arbitrumBridgeReceiver.contract.methods[issuanceFunction](issueAmount).encodeABI()
+        await assertRevert(arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress), "ERR:NOT_ISSUANCE")
+      })
+
+      it('reverts when not called from bridge', async () => {
+        await assertRevert(arbitrumBridgeReceiver[issuanceFunction](issueAmount), "ERR:NOT_FROM_BRIDGE")
+      })
+
+      it('reverts when l2tol2sender is zero address', async () => {
+        await arbitrumOutbox.setL2ToL1Sender(ZERO_ADDRESS)
+        const callDataNewAddress = arbitrumBridgeReceiver.contract.methods[issuanceFunction](issueAmount).encodeABI()
+        await assertRevert(arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress), "ERR:NO_SENDER")
+      })
+    }
+
+    context('updateGovernanceAddress(address _l2GovernanceAddress)', () => {
+
+      itUpdatesBridgeReceiverAsExpected("updateGovernanceAddress", "l2GovernanceAddress")
+
+      itRevertsUpdateFunctionAsExpected("updateGovernanceAddress")
+
+      it('updates governance address twice', async () => {
+        await arbitrumOutbox.setL2ToL1Sender(l2Governance)
+        const callDataNewAddress = arbitrumBridgeReceiver.contract.methods.updateGovernanceAddress(newAddress).encodeABI()
+        await arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress)
+        assert.equal(await arbitrumBridgeReceiver.l2GovernanceAddress(), newAddress, "Incorrect new address")
+
+        await arbitrumOutbox.setL2ToL1Sender(newAddress)
+        const callDataOldAddress = arbitrumBridgeReceiver.contract.methods.updateGovernanceAddress(l2Governance).encodeABI()
+        await arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataOldAddress)
+        assert.equal(await arbitrumBridgeReceiver.l2GovernanceAddress(), l2Governance, "Incorrect old address")
+      })
     })
 
-    it('reverts when l2tol2sender is zero address', async () => {
-      await arbitrumOutbox.setL2ToL1Sender(ZERO_ADDRESS)
-      const callDataNewAddress = arbitrumBridgeReceiver.contract.methods[updateAddressFunction](newAddress).encodeABI()
-      await assertRevert(arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress), "ERR:NO_SENDER")
+    context('updateIssuanceAddress(address _l2IssuanceAddress)', () => {
+      itUpdatesBridgeReceiverAsExpected("updateIssuanceAddress", "l2IssuanceAddress")
+
+      itRevertsUpdateFunctionAsExpected("updateIssuanceAddress")
     })
-  }
 
-  context.only('updateGovernanceAddress(address _l2GovernanceAddress)', () => {
+    context('updateArbitrumInbox(ArbitrumInbox _arbitrumInbox)', () => {
+      itUpdatesBridgeReceiverAsExpected("updateArbitrumInbox", "arbitrumInbox")
 
-    itUpdatesAndRevertsAsExpected("updateGovernanceAddress", "l2GovernanceAddress")
-
-    it('updates governance address twice', async () => {
-      await arbitrumOutbox.setL2ToL1Sender(l2Governance)
-      const callDataNewAddress = arbitrumBridgeReceiver.contract.methods.updateGovernanceAddress(newAddress).encodeABI()
-      await arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress)
-      assert.equal(await arbitrumBridgeReceiver.l2GovernanceAddress(), newAddress, "Incorrect new address")
-
-      await arbitrumOutbox.setL2ToL1Sender(newAddress)
-      const callDataOldAddress = arbitrumBridgeReceiver.contract.methods.updateGovernanceAddress(l2Governance).encodeABI()
-      await arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataOldAddress)
-      assert.equal(await arbitrumBridgeReceiver.l2GovernanceAddress(), l2Governance, "Incorrect old address")
+      itRevertsUpdateFunctionAsExpected("updateArbitrumInbox", "arbitrumInbox")
     })
-  })
 
-  context.only('updateIssuanceAddress(address _l2IssuanceAddress)', () => {
-    itUpdatesAndRevertsAsExpected("updateIssuanceAddress", "l2IssuanceAddress")
-  })
+    context('updateHoneyIssuer(address _issuer)', () => {
+      itUpdatesHoneyAsExpected("updateHoneyIssuer", "issuer")
 
-  context.only('updateArbitrumInbox(ArbitrumInbox _arbitrumInbox)', () => {
-    itUpdatesAndRevertsAsExpected("updateArbitrumInbox", "arbitrumInbox")
+      itRevertsUpdateFunctionAsExpected("updateHoneyIssuer")
+    })
+
+    context('updateHoneyGatewaySetter(address _gatewaySetter)', () => {
+      itUpdatesHoneyAsExpected("updateHoneyGatewaySetter", "gatewaySetter")
+
+      itRevertsUpdateFunctionAsExpected("updateHoneyGatewaySetter")
+    })
+
+    context('updateHoneyGatewayRouter(ArbitrumGatewayRouter _gatewayRouter)', () => {
+      itUpdatesHoneyAsExpected("updateHoneyGatewayRouter", "gatewayRouter")
+
+      itRevertsUpdateFunctionAsExpected("updateHoneyGatewayRouter")
+    })
+
+    context('updateHoneyGateway(ArbitrumCustomGateway _gateway)', () => {
+      itUpdatesHoneyAsExpected("updateHoneyGateway", "gateway")
+
+      itRevertsUpdateFunctionAsExpected("updateHoneyGateway")
+    })
+
+    context('mintHoney(uint256 _amount)', () => {
+      it('mints honey to the correct address', async () => {
+        const mintAmount = bigExp(123, 18)
+        const totalSupplyBefore = await honey.totalSupply()
+        const balanceBefore = await honey.balanceOf(gateway)
+        await arbitrumOutbox.setL2ToL1Sender(l2Issuance)
+        const callDataNewAddress = arbitrumBridgeReceiver.contract.methods.mintHoney(mintAmount).encodeABI()
+
+        await arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress)
+
+        assertBn(await honey.totalSupply(), totalSupplyBefore.add(mintAmount), 'Incorrect totalSupply')
+        assertBn(await honey.balanceOf(gateway), balanceBefore.add(mintAmount), 'Incorrect gateway balance')
+      })
+
+      itRevertsIssuanceFunctionAsExpected("mintHoney")
+    })
+
+    context('burnHoney(uint256 _amount)', () => {
+      it('burns honey from the correct address', async () => {
+        const burnAmount = bigExp(123, 18)
+        const totalSupplyBefore = await honey.totalSupply()
+        const balanceBefore = await honey.balanceOf(gateway)
+        await arbitrumOutbox.setL2ToL1Sender(l2Issuance)
+        const callDataNewAddress = arbitrumBridgeReceiver.contract.methods.burnHoney(burnAmount).encodeABI()
+
+        await arbitrumBridge.executeCall(arbitrumBridgeReceiver.address, callDataNewAddress)
+
+        assertBn(await honey.totalSupply(), totalSupplyBefore.sub(burnAmount), 'Incorrect totalSupply')
+        assertBn(await honey.balanceOf(gateway), balanceBefore.sub(burnAmount), 'Incorrect gateway balance')
+      })
+
+      itRevertsIssuanceFunctionAsExpected("burnHoney")
+    })
   })
 })
